2c2473fc4c90133583c2e730fb69729a26f01dfd
Anik66 <anikkumardas966@gmail.com>
2024-06-12 00:13:11 +0600

    6th commit

--- index.js
+++ index.js
@@ -11,6 +11,52 @@ app.use(cors())
 app.use(express.json())
 
 
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.0wrhevo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    //await client.connect();
+
+    const workcollection = client.db('HrServiceDb').collection('workcollection')
+    //const workcollection = client.db('HrServiceDb').collection('workcollection')
+    const cartcollection = client.db('HrServiceDb').collection('carts')
+    // Send a ping to confirm a successful connection
+    //await client.db("admin").command({ ping: 1 });
+
+    app.get('/work',async(req,res)=>{
+        const result =await workcollection.find().toArray()
+        res.send(result)
+    })
+
+
+
+    app.get('/carts',async(req,res)=>{
+      const cartItem =req.body
+      const result =await cartcollection.insertOne(cartItem)
+      res.send(result)
+    })
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+   // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
 app.get('/',(req,res)=>{
     res.send('okkkkkkk')
 })
